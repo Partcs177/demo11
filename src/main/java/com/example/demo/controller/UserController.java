be8a946435bb6272767e6fa215d76e2c4c98c931
Partcs177 <parshu.tcslearning@gmail.com>
2022-01-21 10:50:29 +0530

    Age added

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +20,9 @@ public class UserController {
 	@Autowired
 	IUserService userService;
 
-	@GetMapping("/user")
-	String getUser() {
-		System.out.println("testing");
-		return "testing";
+	@GetMapping("/user/{id}")
+	Optional<User> getUsers(@PathVariable("id") Integer id) {
+		return userService.getUser(id);
 	}
 
 	@PostMapping("/user") // create
--- src/main/java/com/example/demo/service/IUserService.java
+++ src/main/java/com/example/demo/service/IUserService.java
@@ -5,13 +5,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.UserRepository;
+
 import com.example.demo.entity.User;
+import java.util.Optional;
+
 @Service
 public class IUserService {
-	@Autowired //DI
-	UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
+
 	public void saveUser(User user) {
 		userRepository.save(user);
+	}
+
+	public Iterable<User> getUsers() {
+
+		return userRepository.findAll();
+	}
+
+	public Optional<User> getUser(Integer id) {
+
+		return userRepository.findById(id);
 	}
 
 }

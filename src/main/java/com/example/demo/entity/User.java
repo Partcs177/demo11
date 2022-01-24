cef700e7c496031f52398f4354492c5319c8973b
Partcs177 <parshu.tcslearning@gmail.com>
2022-01-24 15:32:35 +0530

    Address field for new table added

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,9 +1,12 @@
 package com.example.demo.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.validation.constraints.NotBlank;
 
 import org.hibernate.validator.constraints.Range;
@@ -17,7 +20,8 @@ public class User {
 	private String name;
 	@Range(min=0)
 	private Integer age;
-
+	@OneToOne (cascade = CascadeType.ALL)
+	@JoinColumn(name = "address_id", referencedColumnName = "id")
 	public Integer getId() {
 		return id;
 	}
